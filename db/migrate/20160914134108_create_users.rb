1036314fcba5ba8091fc95178527d36db7db3857
Venkata Lakshmi Narasi Bachu <BachuVLNK.Prasanna@eliresearch.com>
2016-09-14 20:42:44 +0530

    added users

---
+++ db/migrate/20160914134108_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+	    t.string :username
+	    t.string :password
+	    t.string :email
+	    t.integer :userid
+		t.integer :taskid
+	    t.string :taskname
+	
+	
+	      t.timestamps
+    end
+  end
+end
